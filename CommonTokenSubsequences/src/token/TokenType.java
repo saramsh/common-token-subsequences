63105d738ad10cf59580fb011bcd0cd5c2af6743
saramsh <saramsht@gmail.com>
2018-01-05 23:07:55 +0330

    initial commit

---
+++ CommonTokenSubsequences/src/token/TokenType.java
@@ -0,0 +1,92 @@
+package token;
+
+public enum TokenType {
+	BlockComment,
+
+	LineComment,
+
+	WhiteSpace,
+
+	Tab,
+
+	NewLine,
+
+	CloseBrace,
+
+	OpenBrace,
+
+	OpeningCurlyBrace,
+
+	ClosingCurlyBrace,
+
+	DoubleConstant,
+
+	IntConstant,
+
+	Plus,
+
+	Minus,
+
+	Multiply,
+
+	Divide,
+
+	Point,
+
+	EqualEqual,
+
+	Equal,
+
+	ExclameEqual,
+
+	Greater,
+
+	Less,
+
+	Static,
+
+	Public,
+
+	Private,
+
+	Int,
+
+	Double,
+
+	Void,
+
+	False,
+
+	True,
+
+	Null,
+
+	Return,
+
+	New,
+
+	Class,
+
+	If,
+
+	While,
+
+	Else,
+
+	Semicolon,
+
+	Comma,
+
+	Identifier;
+
+	/**
+	 * Determines if this token is auxiliary
+	 * 
+	 * @return {@code true} if token is auxiliary, {@code false} otherwise
+	 */
+	public boolean isAuxiliary() {
+		return this == BlockComment || this == LineComment || this == NewLine || this == Tab
+				|| this == WhiteSpace;
+	}
+
+}
