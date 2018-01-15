12e066fa98af6986ab24965f9ca8fa3a7cd0ffd9
saramsh <saramsht@gmail.com>
2018-01-15 12:57:02 +0330

    edit main package

--- CommonTokenSubsequences/src/main/CommonTokenSubsequence.java
+++ CommonTokenSubsequences/src/main/CommonTokenSubsequence.java
@@ -18,22 +18,38 @@ import java.util.regex.Pattern;
 
 import token.TokenType;
 
+import com.googlecode.concurrenttrees.common.PrettyPrinter;
+import com.googlecode.concurrenttrees.radix.node.concrete.DefaultCharSequenceNodeFactory;
+import com.googlecode.concurrenttrees.solver.LCSubstringSolver;
 public class CommonTokenSubsequence {
 
 	public static void main(String[] args) throws AnalyzerException  {
 		// TODO Auto-generated method stub
 			try {
+		  LCSubstringSolver solver = new LCSubstringSolver(new DefaultCharSequenceNodeFactory());
 			String content = new String(Files.readAllBytes(Paths.get("bin\\test.txt")));
 			StringTokenizer defaultTokenizer = new StringTokenizer(content);
 	         Lexer lx=new Lexer();
 	         lx.tokenize(content);
 	         List <Token> result=lx.getTokens();
-
-	         for(int i=0;i<result.size();i++)
+	         solver.add(result.toString());
+	         String content2 = new String(Files.readAllBytes(Paths.get("bin\\test2.txt")));
+	         String content3 = new String(Files.readAllBytes(Paths.get("bin\\test3.txt")));
+	         Lexer lx2=new Lexer();
+	         lx2.tokenize(content2);
+	         List <Token> result2=lx2.getTokens();
+	         solver.add(result2.toString());
+	        Lexer lx3=new Lexer();
+	         lx3.tokenize(content3);
+	         List <String> result3=lx3.getTokensStrings();         
+	         solver.add(result3.toString());
+	         String longestCommonSubstring = solver.getLongestCommonSubstring().toString();
+	         System.out.println(longestCommonSubstring);
+	         for(int i=0;i<result3.size();i++)
 	       
 	        {
 	        	 
-	            System.out.println(result.get(i).getTokenString());
+	          //  System.out.println(result3.get(i).getTokenString());
 	        }
 		  //  System.out.println(content);
 		} catch (IOException e) {
