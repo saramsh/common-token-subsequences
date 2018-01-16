9f447ab613277a8b1a54d30d37b2ed4745ffbbcf
saramsh <saramsht@gmail.com>
2018-01-16 10:44:54 +0330

    change main method to get files in loop

--- CommonTokenSubsequences/src/main/CommonTokenSubsequence.java
+++ CommonTokenSubsequences/src/main/CommonTokenSubsequence.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -25,44 +26,75 @@ public class CommonTokenSubsequence {
 
 	public static void main(String[] args) throws AnalyzerException  {
 		// TODO Auto-generated method stub
+		List<String> listOfFilesContent=new ArrayList<String>();
+		List<String> listOfTokenizedContent=new ArrayList<String>();
 			try {
+				//Read content of files and add them to listOfFilesContent
+				File folder = new File("bin\\2-testcase");
+				File[] listOfFiles = folder.listFiles();
+				
+				for (File file : listOfFiles) {
+					System.out.println(file.getName());
+				    if (file.isFile()) {
+				    	listOfFilesContent.add(new String(Files.readAllBytes(Paths.get("bin\\2-testcase\\"+file.getName()))));						
+				    }
+				}				
 		  LCSubstringSolver solver = new LCSubstringSolver(new DefaultCharSequenceNodeFactory());
-			String content = new String(Files.readAllBytes(Paths.get("bin\\test.txt")));
-			StringTokenizer defaultTokenizer = new StringTokenizer(content);
-	         Lexer lx=new Lexer();
-	         lx.tokenize(content);
-	         List <String> result=lx.getTokensStrings();
-	         String str=result.toString();	       
-	         str=str.replace("[", "");
-	         str=str.replace("]", "");
-	         str=str.replace(",", "");
-	         solver.add(str);
-	         String content2 = new String(Files.readAllBytes(Paths.get("bin\\test2.txt")));
-	         String content3 = new String(Files.readAllBytes(Paths.get("bin\\test3.txt")));
-	         Lexer lx2=new Lexer();
-	         lx2.tokenize(content2);
-	         List <String> result2=lx2.getTokensStrings();
-	         String str2=result2.toString();	       
-	         str2=str2.replace("[", "");
-	         str2=str2.replace("]", "");
-	         str2=str2.replace(",", "");
-	         solver.add(str2);
-	        Lexer lx3=new Lexer();
-	         lx3.tokenize(content3);
-	         List <String> result3=lx3.getTokensStrings();        
-	         String str3=result3.toString();	       
-	         str3=str3.replace("[", "");
-	         str3=str3.replace("]", "");
-	         str3=str3.replace(",", "");
-	         solver.add(str3);
+			//String content = new String(Files.readAllBytes(Paths.get("bin\\test.txt")));
+			StringTokenizer defaultTokenizer;
+			//tokenize fileContent
+			int kk=0;
+			for(String fileContent:listOfFilesContent)
+			{
+				defaultTokenizer= new StringTokenizer(fileContent);
+				//System.out.println("aaaa");
+			//	System.out.println(fileContent);
+				 Lexer lx=new Lexer();
+		         lx.tokenize(fileContent);
+		         System.out.println("bbbbb");
+		         List<String> listOfTokenStrings=lx.getTokensStrings();
+		         System.out.println("ccccc");
+		         String tokenizedContent=listOfTokenStrings.toString();			         
+		         tokenizedContent=tokenizedContent.replace("[", "");
+		         tokenizedContent=tokenizedContent.replace("]", "");
+		         tokenizedContent=tokenizedContent.replace(",", "");
+		         listOfTokenizedContent.add(tokenizedContent);
+		         solver.add(tokenizedContent);	     
+		         System.out.println(kk);
+		         kk++;
+			}
+
+	         
+	         //String content2 = new String(Files.readAllBytes(Paths.get("bin\\test2.txt")));
+	         //String content3 = new String(Files.readAllBytes(Paths.get("bin\\test3.txt")));
+	         //Lexer lx2=new Lexer();
+	         //lx2.tokenize(content2);
+	         //List <String> result2=lx2.getTokensStrings();
+	         //String str2=result2.toString();	       
+	         //str2=str2.replace("[", "");
+	         //str2=str2.replace("]", "");
+	         //str2=str2.replace(",", "");
+	         //solver.add(str2);
+	        //Lexer lx3=new Lexer();
+	         //lx3.tokenize(content3);
+	         //List <String> result3=lx3.getTokensStrings();        
+	         //String str3=result3.toString();	       
+	         //str3=str3.replace("[", "");
+	         //str3=str3.replace("]", "");
+	         //str3=str3.replace(",", "");
+	         //solver.add(str3);
 	         List<String> LongestCommonSubstrings=solver.getLongestCommonSubstrings(solver.getLongestCommonSubstring().toString());
 	         System.out.println(LongestCommonSubstrings.size());
 	         for(int i=0;i<LongestCommonSubstrings.size();i++)
 	       
 	        {
-	        	 
-	           System.out.println(LongestCommonSubstrings.get(i));
+	        	 for(String fileTokenizedContent: listOfTokenizedContent)
+	        	 {
+	           System.out.println(LongestCommonSubstrings.get(i)+" "+LongestCommonSubstrings.get(i).toString().split(" ").length);
+	          System.out.println(fileTokenizedContent+"\n"+count(fileTokenizedContent,LongestCommonSubstrings.get(i) ));
+	        	 }
 	        }
+	        
 		  //  System.out.println(content);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -74,5 +106,19 @@ public class CommonTokenSubsequence {
 		}
 
 	}
+	
+	 public static int count(final String string, final String substring)
+	  {
+	     int count = 0;
+	     int idx = 0;
+
+	     while ((idx = string.indexOf(substring, idx)) != -1)
+	     {
+	        idx++;
+	        count++;
+	     }
+
+	     return count;
+	  }
 
 }
